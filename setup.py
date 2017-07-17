b8cef2e01bb68374c95ae0419cc9aa7c52d65d0c
Yoni Lavi <yoni@codeinstitute.net>
2017-07-17 16:45:50 +0100

    remove inner template directory

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 import os
 from setuptools import setup
- 
+
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
- 
+
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
- 
+
 setup(
     name='reusable-blog-app',
-    version='1.0.0',
+    version='1.0.1',
     packages=['reusable_blog'],
     include_package_data=True,
     license='BSD License',  # example license
